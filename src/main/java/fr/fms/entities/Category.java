665d4d75e765adce942b085e923525e9a8498e57
NicolasG <GuyauN@ad.fms-ea.com>
2023-06-07 14:25:19 +0200

    Article Customer Category

--- src/main/java/fr/fms/entities/Category.java
+++ src/main/java/fr/fms/entities/Category.java
@@ -1,15 +1,13 @@
 package fr.fms.entities;
 
 import java.io.Serializable;
-
-import java.util.Locale.Category;
+import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,19 +15,16 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor @ToString
-public class Article implements Serializable{
+public class Category implements Serializable{
 	private static final long serialVersionUID=1L;
-	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
-	private Long id;
-	private String description;
-	private double unitaryPrice;
-	private String brand;
-	@ManyToOne
-	private Category category;
-	//@OneToMany(mappedBy = "orderedItem")
-	//private Collection<OrderedItem> orderedItems;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	@OneToMany(mappedBy ="category" )
+	private Collection<Article> articles;
 
+	
+	
+	
 }
-	
 
-	
--- src/main/java/fr/fms/entities/Customer.java
+++ src/main/java/fr/fms/entities/Customer.java
@@ -2,34 +2,32 @@ package fr.fms.entities;
 
 import java.io.Serializable;
 
-import java.util.Locale.Category;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
+
+import org.apache.catalina.User;
+import org.hibernate.annotations.BatchSize;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 @Entity @Data @NoArgsConstructor @AllArgsConstructor @ToString
-public class Article implements Serializable{
+public class Customer implements Serializable{
 	private static final long serialVersionUID=1L;
-	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
-	private Long id;
-	private String description;
-	private double unitaryPrice;
-	private String brand;
-	@ManyToOne
-	private Category category;
-	//@OneToMany(mappedBy = "orderedItem")
-	//private Collection<OrderedItem> orderedItems;
-
+	@Id@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String firstName;
+	private String lastName;
+	//@Size(min = 5,max = 60)
+	private String email;
+	private String address;
+	private String phone;	
+	//@OneToMany(mappedBy="order")
+	//private Collection<Order> orders;
+	
 }
-	
-
-	
