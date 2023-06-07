2104be7048724246890da4248844752ed19e3b34
Christian PAZMANY <PazmanyC@ad.fms-ea.com>
2023-06-07 12:26:26 +0200

    create the tables Order and OrderItem

---
+++ src/main/java/fr/fms/entities/Order.java
@@ -0,0 +1,34 @@
+package fr.fms.entities;
+
+
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor @ToString @Table(name = "orders")
+public class Order implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date date;
+	private double totalAmount;
+
+	
+	@OneToMany(mappedBy = "order")
+	private Collection<OrderItem> orderItems;
+}
