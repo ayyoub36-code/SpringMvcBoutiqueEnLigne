1f83007da5e9beac2be23d153d04910163e98a46
Ayyoub MEHDIOUI <MehdiouiA@ad.fms-ea.com>
2023-06-13 10:13:09 +0200

    todo

--- src/main/java/fr/fms/web/CartController.java
+++ src/main/java/fr/fms/web/CartController.java
@@ -7,8 +7,16 @@ public class CartController {
 
 	// panier affichage CartController (structure d enregistrement List(ArrayList)
 	// => panier) et recuperer le cart.size => Navbarre
+	
+	
 	// add to cart button addToCart(index) => OrderItem => quantity ++
+	
+	
+	
 	// delete from cart
 
+	
+	
+	
 	// enregistrer la commande
 }
